1374753e7bcf97958a4810e7891c49a3d4ffd8bf
David Rodriguez <davidrodriguez@Davids-MacBook-Pro.local>
2023-05-16 19:28:47 -0400

    basic js added

---
+++ index.js
@@ -0,0 +1,7 @@
+// importing the requires
+const inquirer = require("inquirer");
+const svgwrite = require("svgwrite");
+const { writeFilesync } = require("fs");
+
+//function for logo data
+function generateLogo(logoData);
