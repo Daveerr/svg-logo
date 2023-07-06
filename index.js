b611d17f3624d46641df9f7b1557ddb0b766e5d1
David Rodriguez <Daviderrr10@gmail.com>
2023-07-06 12:54:59 -0400

    shapes.js / index.js

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
 // importing the requires
-const inquirer = require("inquirer");
+const inquirer = import("inquirer");
 const fs = require("fs");
 const { createSVG } = require("svg-builder");
+const { triangle, circle, square } = require("./lib/shapes");
+const shape = require("./lib/shapes");
 
 // promts for logo details
 function generateLogo() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "text",
-      message: "Enter the text for your logo:",
-    },
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter the text for your logo:",
+      },
 
-    {
-      type: "list",
-      name: "shape",
-      message: "select a shape for your logo",
-      choices: ["square", "triangle", "circle"],
-    },
+      {
+        type: "list",
+        name: "shape",
+        message: "select a shape for your logo",
+        choices: ["square", "triangle", "circle"],
+      },
 
-    {
-      type: "input",
-      name: "color",
-      message: "Color for your logo",
-    },
+      {
+        type: "input",
+        name: "color",
+        message: "Color for your logo",
+      },
 
-    {
-      type: "input",
-      name: "filename",
-      message: "filname to save the SVG",
-    },
-  ]);
+      {
+        type: "input",
+        name: "filename",
+        message: "filname to save the SVG",
+      },
+    ])
+    .then((answers) => {
+      let shape;
+      const shapeColor = answers.color;
+
+      if (answers.shape === "circle") {
+        shape = new circle(shapeColor);
+      } else if (answers.shape === "triangle") {
+        shape = new triangle(shapeColor);
+      } else if (answers.shape === "square") {
+        shape = new square(shapeColor);
+      }
+      // Generate the SVG logo
+      const svgLogo = createSVG()
+        .size(200, 200)
+        .rect(0, 0, 200, 200)
+        .fill(answers.color)
+        .element(answers.shape.toLowerCase(), { cx: 100, cy: 100, r: 50 })
+        .text(answers.text, {
+          x: 100,
+          y: 100,
+          fill: "#ffffff",
+          "text-anchor": "middle",
+          "alignment-baseline": "central",
+        })
+        .end();
+      // Save the SVG to a file
+      const filename = `${answers.filename}.svg`;
+      fs.writeFileSync(filename, svgLogo);
+      console.log(`Logo saved to ${filename}`);
+    })
+    .catch((error) => {
+      console.error("An error occurred to save logo:", error);
+    });
 }
